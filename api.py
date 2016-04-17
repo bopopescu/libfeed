80a92b1e0e650439914209a0990bfc9e37d24120
leslierice <leslierice@utexas.edu>
2016-04-17 12:48:09 -0500

    deleting file

--- api.py
+++
@@ -1,8 +0,0 @@
-from application import application, logger
-from flask import jsonify
-from flask.ext.stormpath import StormpathError, StormpathManager, User, login_required, login_user, logout_user, user
-
-@application.route('/api/get_user')
-@login_required
-def get_user(body):
-    return jsonify({'user': user})
