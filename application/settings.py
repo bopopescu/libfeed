34a7f9f4f135e8fb22e315a5f734f96ed64065dc
leslierice <leslierice@utexas.edu>
2016-06-21 09:38:36 -0500

    test deploy on aws environ vars

---
+++ application/settings.py
@@ -0,0 +1,7 @@
+import os
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
+AWS_SECRET_KEY = os.environ['AWS_SECRET_KEY']
+GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
